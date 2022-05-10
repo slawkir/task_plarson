9b1c6c029c268352bddddd4a8f97af9c3c0bf186
slawkir <slawkir1984@gmail.com>
2022-05-10 19:21:41 +0400

    fix: correction of function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,4 +1,20 @@
-window.addEventListener('load', function () { 
-	alert('Страница загружена');
-})
+function changeState(cb) {
+	document.addEventListener('readystatechange', () => {
+		if (document.readyState === 'interactive') {
+			cb();
+			document.addEventListener('DOMContentLoaded', () => {
+				cb()
+			});
+		}
+	})
+}
 
+const attentionFunction = () => {
+	alert('Внимание!');
+}
+
+changeState(attentionFunction)
+
+document.addEventListener('DOMContentLoaded', () => {
+	alert('Dom загрузился!!!');
+})
